6eab6a1e77b511bd7460b5d8b04b89653ecb82f7
Gabrielle-Ana <91814638+Gabrielle-Ana@users.noreply.github.com>
2023-08-28 22:27:25 -0300

    Add files via upload

---
+++ atividade3/SistemaDeGerenciamentoDeEstoque.java
@@ -0,0 +1,42 @@
+package atividade3;
+
+public class SistemaDeGerenciamentoDeEstoque {
+    
+    public static void main(String[] args) {
+         private String nome;
+        private int quantidade;
+    }
+
+    public static void teste(String nome, int quantidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Item [nome=" + nome + ", quantidade=" + quantidade + "]";
+    }
+    }
+}
+
+
+public Item {
+
+   
+}
